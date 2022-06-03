b70ab7759388dc1c42537a31624e0d36893a1d82
JohnnyLin97 <jy.br.lin@gmail.com>
2022-06-03 23:14:49 +0800

    Migrate to db

--- Ticketing_System/ticket/models.py
+++ Ticketing_System/ticket/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-from Ticketing_System.ticket.custom_fields import SeparatedValuesField
-import custom_fields
 
 # Create your models here.
 
@@ -10,12 +8,12 @@ class TRANSACTION(models.Model):
     name = models.CharField
     phone = models.DecimalField
     email = models.CharField
-    seat = SeparatedValuesField()
+    
 
 # Seats of performance
 class SEATS(models.Model):
-    area = models.CharField(max_length=2) 
-    num = models.DecimalField(max_digits=2)
+    id = models.CharField(max_length=45).primary_key
+    area = models.CharField(max_length=1) 
+    num = models.CharField(max_length=2)
     booked = models.BooleanField
-    transactionId = models.ForeignKey(TRANSACTION, on_delete=models.SET_NULL)
-
+    transactionId = models.CharField(max_length=45)
