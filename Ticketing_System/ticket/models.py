abc4cfbd5c912f8f4c30e3206143c5cc9acb12e8
JohnnyLin97 <jy.br.lin@gmail.com>
2022-06-04 17:26:36 +0800

    Migrate db format

--- Ticketing_System/ticket/models.py
+++ Ticketing_System/ticket/models.py
@@ -1,19 +1,26 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
+from django_mysql.models import ListCharField
 
 # Create your models here.
 
 # Transaction record
-class TRANSACTION(models.Model):
+class Transaction(models.Model):
     transactionId = models.CharField
-    name = models.CharField
-    phone = models.DecimalField
-    email = models.CharField
+    name = models.CharField(max_length=45)
+    phone = models.CharField(max_length=45)
+    email = models.CharField(max_length=45)
+    seat = ListCharField(
+        base_field = models.CharField(max_length=45),
+        size = 6,
+        _MAX_LENGTH=(6*45)
+    )
     
 
 # Seats of performance
-class SEATS(models.Model):
-    id = models.CharField(max_length=45).primary_key
+class Event1_seats(models.Model):
+    seatId = models.CharField(max_length=45).primary_key
     area = models.CharField(max_length=1) 
     num = models.CharField(max_length=2)
-    booked = models.BooleanField
+    booked = models.BooleanField(default=False)
     transactionId = models.CharField(max_length=45)
