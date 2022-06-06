384d7063eba35dcad79ed80d05915151478f8212
JohnnyLin97 <jy.br.lin@gmail.com>
2022-06-06 17:46:23 +0800

    Add retrieve API

--- Ticketing_System/ticket/db_helper.py
+++ Ticketing_System/ticket/db_helper.py
@@ -1,9 +1,48 @@
+from .models import Event1_seats
+import random
+from django.core import serializers
 
+def create_seat_table():
+    for i in range(1,21):
 
-from Ticketing_System.ticket.models import Event1_seats
+        seatId = random.randrange(100000000000,999999999999)
+        area = 'J'
+        num = str(i)
+        booked = False
+        transactionId = ''
 
+        unit = Event1_seats.objects.create(
+            id = seatId,
+            area = area,
+            num = num,
+            booked = booked,
+            transactionId = transactionId
+        )
+        
+        unit.save()
 
-def search_seats(col_name, keyword):
-    unit = Event1_seats.objects.get(col_name = keyword)
+def retrieve_all_seats():
+    all_units = Event1_seats.objects.all()
 
-    unit = Event1_seats.objects.all
+    #Convert querySet to list
+    all_units_list = list(all_units)
+
+    all_seats = []
+    for i in all_units_list:
+        all_seats.append(unit_to_dic(i))
+
+    return all_seats
+
+def unit_to_dic(unit):
+    dict = {}
+    dict['id'] = unit.id
+    dict['area'] = unit.area
+    dict['num'] = unit.num
+    dict['transactionId'] = unit.transactionId
+
+    if unit.booked:
+        dict['booked'] = True
+    else:
+        dict['booked'] = False
+
+    return dict
--- Ticketing_System/ticket/views.py
+++ Ticketing_System/ticket/views.py
@@ -1,27 +1,13 @@
 from .models import Event1_seats
 from django.http import HttpResponse
 import json
+import random
+from .db_helper import retrieve_all_seats
 
 # Create your views here.
 def retrieve(request):
     try:
-        #unit = Event1_seats.objects.get(id = 5494156464648)
-        unit = Event1_seats.objects.get(booked=False)
-        print(unit)
-        '''
-        seatId = 549415637700
-        area = 'A'
-        num = '1'
-        booked = False
-        transactionId = ''
-        unit = Event1_seats.objects.create(
-            id = seatId,
-            area = area,
-            num = num,
-            booked = booked,
-            transactionId = transactionId
-        )
-        unit.save()'''
+        all_seats = retrieve_all_seats()
     
     except Exception as e: 
         print(e)
@@ -30,4 +16,4 @@ def retrieve(request):
     response_data['result'] = 'error'
     response_data['message'] = 'Some error message'
 
-    return HttpResponse(json.dumps(response_data), content_type="application/json")
+    return HttpResponse(json.dumps(all_seats), content_type="application/json")
