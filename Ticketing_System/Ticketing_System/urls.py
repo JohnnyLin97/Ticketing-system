d6db644017ac89163619fcbbd27ad1bab74625ad
JohnnyLin97 <jy.br.lin@gmail.com>
2022-06-12 14:20:57 +0800

    Change API method to POST

--- Ticketing_System/Ticketing_System/urls.py
+++ Ticketing_System/Ticketing_System/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from ticket import views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('retrieve/seat', views.retrieve_seats),   #Get seat table
--- Ticketing_System/ticket/views.py
+++ Ticketing_System/ticket/views.py
@@ -1,6 +1,6 @@
 import email
 from .models import Event1_seats
-from django.http import HttpResponse, HttpResponseNotAllowed, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseForbidden, HttpResponseNotAllowed, HttpResponseNotFound
 import json
 from .db_helper import book_seat, check_seat_state, get_transaction_db, retrieve_all_seats, create_transaction_db, retrieve_all_transactions
 
@@ -16,34 +16,43 @@ def retrieve_seats(request):
 
 
 def get_transaction(request):
-    transaction_id = json.loads(request.body)["id"]
-    
-    try:
-        transaction = get_transaction_db(transaction_id)
-        return HttpResponse(json.dumps(transaction), content_type="application/json")
-    
-    except Exception as e:
-        print(e)
-        return HttpResponseNotFound(json.dumps("Can't find the transaction"), content_type="application/json")
+    if request.method == "POST": 
+        transaction_id = json.loads(request.body)["id"]
+        
+        try:
+            transaction = get_transaction_db(transaction_id)
+            return HttpResponse(json.dumps(transaction), content_type="application/json")
+        
+        except Exception as e:
+            print(e)
+            return HttpResponseNotFound(json.dumps("Can't find the transaction"), content_type="application/json")
+
+    else:
+        return HttpResponseForbidden()
 
 def create_transaction(request):
-    request_json = json.loads(request.body)
+    if request.method == "POST": 
+        request_json = json.loads(request.body)
 
-    email = request_json["email"]
-    name = request_json["name"]
-    phone = request_json["phone"]
-    seat = request_json["seat"]
+        email = request_json["email"]
+        name = request_json["name"]
+        phone = request_json["phone"]
+        seat = request_json["seat"]
 
-    if check_seat_state(seat):
-        return HttpResponseNotFound(json.dumps("Seat is already reserved"), content_type="application/json")
+        if check_seat_state(seat):
+            return HttpResponseNotFound(json.dumps("Seat is already reserved"), content_type="application/json")
 
-    id = create_transaction_db(email, name, phone, seat)
-    dict = {}
-    dict['id'] = id
+        id = create_transaction_db(email, name, phone, seat)
+        dict = {}
+        dict['id'] = id
 
-    book_seat(seat, id)
+        book_seat(seat, id)
 
-    return HttpResponse(json.dumps(dict), content_type="application/json")
+        return HttpResponse(json.dumps(dict), content_type="application/json")
+
+    else:
+        return HttpResponseForbidden()
+
 
 def retrieve_transaction(request):
     try:
